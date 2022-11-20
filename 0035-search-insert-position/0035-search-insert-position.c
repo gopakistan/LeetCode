7eec47454f628e7e02a5b546f3dec804c89952ba
gopakistan <101689049+gopakistan@users.noreply.github.com>
2022-11-19 23:45:45 -0500

    Time: 7 ms (63.35%), Space: 6.1 MB (53.38%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.c
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -1,11 +1,7 @@
 int searchInsert(int* nums, int numsSize, int target){
     int i = 0;
     for (i = 0; i < numsSize; i++){
-        if (target == nums[i]) return i;
-        if (nums[i] > target) break;
+        if (target <= nums[i] ) return i;
     }
-    
     return i;
-    
-    
 }
