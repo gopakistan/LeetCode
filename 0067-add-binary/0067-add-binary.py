8a9f6f66d7202442ecad678a5d5a160ec6f5900c
gopakistan <101689049+gopakistan@users.noreply.github.com>
2022-11-27 00:11:49 -0500

    Time: 56 ms (16.72%), Space: 13.6 MB (48.45%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -10,26 +10,19 @@ class Solution(object):
         while (len(a) > len(b)):
             b = '0' + b
         while (len(a) < len(b)):
-            a = '0' + a       
-        a = a[::-1]
-        b = b[::-1]
-        #print(a, b)
-        #ab = int(a[0]) + int(b[0])
-        #print(ab)
+            a = '0' + a
+            
         sum = ''
         c = 0
         tempsum = 0
-        for i in range(len(a)):
-            #print(i, a[i], b[i])
+        for i in range(len(a)-1, -1, -1):
             tempsum = c + int(a[i]) + int(b[i])
-            #print('     =',tempsum)
             c = 0
             if tempsum > 1:
                 c = 1
                 tempsum -= 2
-                #print("CARRY")
             sum = str(tempsum) + sum
-            #print('SUM:', sum)
+            
         if c == 1:
             sum = str(c) + sum 
         return sum
